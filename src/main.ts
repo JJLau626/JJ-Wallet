61d053492146d76545d877a8a8b94f226197ac4d
jjlao <348211201@qq.com>
2023-07-04 23:05:42 +0800

    build: 项目初始化

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+// tailwind.css 已经带有重置 css 的能力。
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
